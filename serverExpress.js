b1b5a7a26ac40dbc2a17585047cdec247af4da52
Dylan Cunningham <gdylanc@gmail.com>
2017-08-01 14:29:48 -0700

    send individual guest records from the database

--- serverExpress.js
+++ serverExpress.js
@@ -10,22 +10,42 @@ var port = process.env.PORT || 8000;
 
 app.disable('x-powered-by');
 
-app.get('/guests', function(req, res){
-  fs.readFile(guestsPath, 'utf8', function(err, guestsJSON){
-    if (err){
+app.get('/guests', function(req, res) {
+  fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
+    if (err) {
       console.error(err.stack);
       return res.sendStatus(500);
     }
+
     var guests = JSON.parse(guestsJSON);
 
     res.send(guests);
   });
 });
 
-app.use(function(req, res){
-  res.sendStatus(404);
-})
+app.get('/guests/:id', function(req, res) {
+  fs.readFile(guestsPath, 'utf8', function(err, guestsJSON){
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
 
-app.listen(port, function(){
-  console.log('listening on port: ', port);
-})
+    var id = Number.parseInt(req.params.id);
+    var guests = JSON.parse(guestsJSON);
+
+    if (id < 0 || id >= guests.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    res.set('Content-Type', 'text/plain');
+    res.send(guests[id]);
+  });
+});
+
+app.use(function(req, res) {
+  res.sendStatus(404);
+});
+
+app.listen(port, function() {
+  console.log('Listening on port', port);
+});
